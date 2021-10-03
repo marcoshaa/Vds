f681922bba1284066bb7545b46331c35837d0e21
marcoshaa <m.andrade.assis@gmail.com>
2021-10-03 19:48:47 -0300

    start project

---
+++ backend/src/main/java/com/dev/devFrist/DevFristApplication.java
@@ -0,0 +1,13 @@
+package com.dev.devFrist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevFristApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevFristApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dev/devFrist/DevFristApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.devFrist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevFristApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
